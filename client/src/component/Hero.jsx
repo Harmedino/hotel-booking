edbca7890a419b85b8756c7093983d65585043e5
Harmedino <adamsahmed191@gmail.com>
2026-01-06 13:06:09 +0100

    hero form

--- client/src/component/Hero.jsx
+++ client/src/component/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Hero = () => {
   return (
@@ -19,23 +20,7 @@ const Hero = () => {
       <form className="bg-white text-gray-500 rounded-lg px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto">
         <div>
           <div className="flex items-center gap-2">
-            <svg
-              className="w-4 h-4 text-gray-800"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 10h16M8 14h8m-4-7V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"
-              />
-            </svg>
+            <img src={assets.calenderIcon} alt="calender" className="h-4" />
             <label htmlFor="destinationInput">Destination</label>
           </div>
           <input
