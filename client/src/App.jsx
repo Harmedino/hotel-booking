5154ab4ca6391e95627483d7cc58d29fd1078061
Harmedino <adamsahmed191@gmail.com>
2026-01-06 12:29:47 +0100

    hero moving

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
-import { useLocation } from "react-router-dom"
-import Navbar from "./component/Navbar"
-import Home from "./component/pages/Home"
-
+import { useLocation, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./component/pages/Home";
 
 function App() {
- const isOwnerPath = useLocation().pathname.includes('owner')
+  const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
     <>
       {!isOwnerPath && <Navbar />}
-      <Home/>
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
